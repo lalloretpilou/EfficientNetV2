f170e6e203613e8da0031643b5ed31cf36769f8f
L'ALLORET Pierre-Louis <42711915+lalloretpilou@users.noreply.github.com>
2024-12-20 10:37:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 
 # Charger le modèle pré-entraîné
-MODEL_PATH = "model_transfer_learning_EfficientNetV2.keras"
+MODEL_PATH = "model_transfer_learning_EfficientNetV2.h5"
 model = tf.keras.models.load_model(MODEL_PATH)
 
 # Classes des chiens
